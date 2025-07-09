2c15dfc8d434f4e81a84a15ca7fefc70307611f9
laujuvi <laujuvi@hotmail.com>
2025-07-08 22:23:45 -0300

    css chat

--- src/Component/ContactItem/ContactItem.jsx
+++ src/Component/ContactItem/ContactItem.jsx
@@ -1,22 +1,46 @@
 import React from "react"
-import { Link} from "react-router"
+import { Link } from "react-router"
+import './ContactItem.css'
 
-const ContactItem = ({name, id, last_time_connected, img, last_message, unread_messages}) => {
-    
+const ContactItem = ({ name, id, last_time_connected, img, last_message, unread_messages }) => {
+
     return (
-        <Link to={`/contacts/${id}/messages`}>
-            {/* <a href="/home"> */}{/* el elemento <a></a> Recarga la pagina */}
-            <img src={img} alt={`${name} image profile`} />
-            <h3>{name}</h3>
-            <div>
-                {last_message.text}
+
+
+        <Link style={{ textDecoration: 'none' }} to={`/contacts/${id}/messages`}>
+            <div className="contenedor-card-icons">
+
+                <div className="img-msj">
+
+
+
+                    <div className="card-icon"> <img src={img} alt={`${name} image profile`} />
+                    </div>
+                    <div className="img-msj-contact">
+
+                        <div className="contacto-msj">
+                            <div className="card-title">{name}</div>
+                            <div className="card-description">{last_message.text}</div>
+                        </div>
+
+
+
+
+                        <div className="horario-notif">
+                            <span className="hora">10:00</span>
+                            <span className="notif">{unread_messages}</span>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
-            <span>{unread_messages}</span>
-            <span>Ultima conexion: {last_time_connected}</span>
-            <hr/>
+
+
+
         </Link>
-        
-        
+
+
     )
 }
 
