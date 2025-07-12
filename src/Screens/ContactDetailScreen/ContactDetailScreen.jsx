9e044bef036074d2f3670a7e50c82e3c2e0c94f7
laujuvi <marianahokama@gmail.com>
2025-07-12 01:57:12 -0300

    puchiado

--- src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
+++ src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect } from 'react'
-import { useParams } from 'react-router'
+import { useParams, useNavigate } from 'react-router'
 import { ContactDetailContext } from '../../Context/ContactDetailContext'
 import LoaderSpinner from '../../Component/LoaderSpinner/LoaderSpinner'
 import './ContactDetailScreen.css'
+import { Link } from "react-router"
 
 
 
 export default function ContactDetailScreen() {
     const { contact_id } = useParams()
     const { loadContact, contact_detail, isLoadingContactDetail } = useContext(ContactDetailContext)
+      const navigate = useNavigate()
 
 
     useEffect(
@@ -24,17 +26,19 @@ export default function ContactDetailScreen() {
     return (
 
 
-        
+
 
         <div className="contact-detail-screen">
-             
 
-               <div className="header-contact-detail header-contact-icon">
 
+
+
+                            <div className="header-contact-detail">
+                <button onClick={() => navigate(-1)} className="go-back-button" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
-</svg>
-
+                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+                </svg>
+                </button>
             </div>
 
             <div className="contact-detail">
